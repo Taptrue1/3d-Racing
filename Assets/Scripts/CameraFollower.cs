1703506e7025bf0423d1f418e8ae138be5569642
Taptrue <82027635+Taptrue1@users.noreply.github.com>
2022-02-21 19:52:42 +0300

    Написал скрипт слежения за машиной

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _speed;
+    [SerializeField] private Vector3 _offset;
+
+    private Vector3 _velocity;
+
+    private void Start()
+    {
+        transform.position = _target.position + _offset;
+    }
+    private void FixedUpdate() => Move();
+
+    private void Move()
+    {
+        var cameraPosition = _target.position + _offset;
+        transform.position = Vector3.SmoothDamp(transform.position, cameraPosition, ref _velocity, _speed);
+    }
+}
