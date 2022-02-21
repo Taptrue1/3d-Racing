1e5a36bb77f2e5128e9612c3e5e51399fec8e23d
Taptrue <82027635+Taptrue1@users.noreply.github.com>
2022-02-21 19:51:57 +0300

    Добавил плагины и настроил машину

---
+++ Assets/Plugins/EasySuspension/EasySuspension.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode()]
+public class EasySuspension : MonoBehaviour 
+{
+	[Range(0, 20)]
+	[SerializeField] private float naturalFrequency = 10;
+	[Range(0, 3)]
+	[SerializeField] private float dampingRatio = 0.8f;
+	[Range(-1, 1)]
+	[SerializeField] private float forceShift = 0.03f;
+	[SerializeField] private bool setSuspensionDistance = true;
+
+	private WheelCollider[] wheels;
+
+    private void Awake()
+    {
+        wheels = GetComponentsInChildren<WheelCollider>();
+    }
+    private void Update () {
+		// work out the stiffness and damper parameters based on the better spring model
+		foreach (WheelCollider wheel in wheels) {
+			JointSpring spring = wheel.suspensionSpring;
+
+			spring.spring = Mathf.Pow(Mathf.Sqrt(wheel.sprungMass) * naturalFrequency, 2);
+			spring.damper = 2 * dampingRatio * Mathf.Sqrt(spring.spring * wheel.sprungMass);
+
+			wheel.suspensionSpring = spring;
+
+			Vector3 wheelRelativeBody = transform.InverseTransformPoint(wheel.transform.position);
+			float distance = GetComponent<Rigidbody>().centerOfMass.y - wheelRelativeBody.y + wheel.radius;
+
+			wheel.forceAppPointDistance = distance - forceShift;
+
+			// the following line makes sure the spring force at maximum droop is exactly zero
+			if (spring.targetPosition > 0 && setSuspensionDistance)
+				wheel.suspensionDistance = wheel.sprungMass * Physics.gravity.magnitude / (spring.targetPosition * spring.spring);
+		}
+	}
+
+// uncomment OnGUI to observe how parameters change
+
+/*
+	public void OnGUI()
+	{
+		foreach (WheelCollider wc in GetComponentsInChildren<WheelCollider>()) {
+			GUILayout.Label (string.Format("{0} sprung: {1}, k: {2}, d: {3}", wc.name, wc.sprungMass, wc.suspensionSpring.spring, wc.suspensionSpring.damper));
+		}
+
+		var rb = GetComponent<Rigidbody> ();
+
+		GUILayout.Label ("Inertia: " + rb.inertiaTensor);
+		GUILayout.Label ("Mass: " + rb.mass);
+		GUILayout.Label ("Center: " + rb.centerOfMass);
+	}
+*/
+
+}
---
+++ Assets/Plugins/EasySuspension/RearWheelDrive.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class RearWheelDrive : MonoBehaviour 
+{
+	[SerializeField] private float _maxAngle = 30;
+	[SerializeField] private float _maxTorque = 300;
+	[SerializeField] private GameObject _wheelShape;
+	[SerializeField] private WheelCollider[] _rightWheels;
+	[SerializeField] private WheelCollider[] _leftWheels;
+
+	private List<WheelCollider> _allWheels;
+
+	private void Start()
+	{
+		_allWheels = new List<WheelCollider>(_rightWheels.Length + _leftWheels.Length);
+		var isNeedToCreateWheels = _wheelShape != null;
+
+		foreach (var wheel in _rightWheels)
+			_allWheels.Add(wheel);
+		foreach (var wheel in _leftWheels)
+			_allWheels.Add(wheel);
+
+		if(isNeedToCreateWheels)
+			foreach (var wheel in _allWheels)
+				CreateWheel(wheel);
+	}
+	private void Update()
+	{
+		float angle = _maxAngle * Input.GetAxis("Horizontal");
+		float torque = _maxTorque * Input.GetAxis("Vertical");
+
+		foreach(var wheel in _allWheels)
+        {
+			var isRightWheel = IsRightWheel(wheel);
+			UpdateWheel(wheel, angle, torque, isRightWheel);
+		}
+	}
+
+	private void UpdateWheel(WheelCollider wheel, float angle, float torque, bool isRight)
+    {
+		SetWheelSteerAngle(wheel, angle, isRight);
+		SetWheelTorque(wheel, torque, isRight);
+
+		if (_wheelShape)
+		{
+			wheel.GetWorldPose(out Vector3 position, out Quaternion rotation);
+
+			Transform shapeTransform = wheel.transform.GetChild(0);
+			shapeTransform.position = position;
+			shapeTransform.rotation = rotation;
+			shapeTransform.localScale = new Vector3(1, 1, 1);
+		}
+	}
+	private void SetWheelSteerAngle(WheelCollider wheel, float angle, bool isRight)
+	{
+		var isFrontWheel = wheel.transform.localPosition.z > 0;
+
+		if (isRight)
+			wheel.steerAngle = isFrontWheel ? 180 + angle : 180;
+		else
+			wheel.steerAngle = isFrontWheel ? angle : 0;
+	}
+	private void SetWheelTorque(WheelCollider wheel, float torque, bool isRight)
+    {
+		var isBackWheel = wheel.transform.localPosition.z < 0;
+
+		if (isBackWheel)
+			wheel.motorTorque = isRight ? -torque : torque;
+    }
+	private bool IsRightWheel(WheelCollider wheel)
+	{
+		foreach (var rightWheel in _rightWheels)
+			if (wheel == rightWheel)
+				return true;
+
+		return false;
+	}
+	private void CreateWheel(WheelCollider wheel)
+    {
+		var ws = GameObject.Instantiate(_wheelShape);
+		ws.transform.parent = wheel.transform;
+	}
+}
